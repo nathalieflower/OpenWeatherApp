ad656201509d3c528618940000150b1e1d33b7be
Nathalie Flower <nathalie.flower@hotmail.co.uk>
2019-06-09 22:41:53 +0100

    initial commit

---
+++ WeatherApp/Tests/EndToEndTests.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class EndToEndTests
+    {
+        public const string apiKey = "f6ad1287a07ddeb1afbde1fe0b99db05";
+
+        HttpClient Client = new HttpClient(); 
+
+        [Fact]
+        public async Task GetWeatherForLondon()
+        {
+            //arrange
+            var baseUri = $"https://api.openweathermap.org/data/2.5/weather?q=London&APPID={apiKey}";
+
+            //act
+            var httpResponse = await Client.GetAsync(baseUri);
+
+            //assert
+
+            var statusCode = httpResponse.StatusCode;
+            Assert.Equal(HttpStatusCode.OK, statusCode);
+
+            var result = await httpResponse.Content.ReadAsStringAsync();
+            var jsonResult = JsonConvert.DeserializeObject<dynamic>(result);
+            Assert.Contains("weather", jsonResult);
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public const string apiKey = "f6ad1287a07ddeb1afbde1fe0b99db05";
+        public async Task<ActionResult> Index(string search)
+        {
+            return View(await GetWeather(search));
+        }
+
+        public async Task<WeatherDetails> GetWeather(string search)
+        {
+
+            if (search == null)
+            {
+                search = "London";
+            }
+            WeatherDetails weatherDetails = null;
+            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={search}&units=metric&APPID={apiKey}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    weatherDetails = JsonConvert.DeserializeObject<WeatherDetails>(jsonResponse);
+                }
+
+                else
+                {
+                    throw new Exception("Location Not Valid");
+                }
+                return weatherDetails;
+            }
+        }
+    }
+}
